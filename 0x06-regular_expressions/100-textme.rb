9f7eb124efa7ec3c86dcba041e84d1871b08eb79
Bright <bright.sablah@outlook.com>
2024-05-04 22:00:52 +0000

    textme ruby

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-
-def extract(text)
-	sender = text.match(/\[from:(\[^[\]]*)\]/)[1]
-	receiver = text.match(/\[to:(\[^[\]]*)\]/)[1]
-	flags = text.match(/\[flags:(\[^[\]]*)\]/)[1]
-	"#{sender},#{receiver},#{flags}"
-end
-
-puts extract(ARGV[0])
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
